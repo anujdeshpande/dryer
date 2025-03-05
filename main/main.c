073f1d3054c74f86fbde448d6add1ffa08718778
Anuj Deshpande <anuj@makerville.io>
2025-03-05 11:44:18 +0800

    Add basic fan control - no fade

--- main/main.c
+++ main/main.c
@@ -1,17 +1,3 @@
-/*
- * SPDX-FileCopyrightText: 2022 Espressif Systems (Shanghai) CO LTD
- *
- * SPDX-License-Identifier: Unlicense OR CC0-1.0
- */
-/* HTTP File Server Example
-
-   This example code is in the Public Domain (or CC0 licensed, at your option.)
-
-   Unless required by applicable law or agreed to in writing, this
-   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-   CONDITIONS OF ANY KIND, either express or implied.
-*/
-
 #include "esp_event.h"
 #include "esp_log.h"
 #include "esp_netif.h"
@@ -19,17 +5,95 @@
 #include "nvs_flash.h"
 #include "protocol_examples_common.h"
 #include "file_serving_example_common.h"
+#include "esp_console.h"
+#include "cmd_system.h"
+#include "driver/ledc.h"
 
 /* This example demonstrates how to create file server
  * using esp_http_server. This file has only startup code.
  * Look in file_server.c for the implementation.
  */
 
-static const char *TAG = "example";
+static const char *TAG = "dryer";
+#define PROMPT_STR "dryer"
+
+#define LEDC_TIMER              LEDC_TIMER_0
+#define LEDC_MODE               LEDC_LOW_SPEED_MODE
+#define LEDC_OUTPUT_IO          (5) // Define the output GPIO
+#define LEDC_CHANNEL            LEDC_CHANNEL_0
+#define LEDC_DUTY_RES           LEDC_TIMER_13_BIT // Set duty resolution to 13 bits
+#define LEDC_DUTY               (4096) // Set duty to 50%. (2 ** 13) * 50% = 4096
+#define LEDC_FREQUENCY          (4000) // Frequency in Hertz. Set frequency at
+
+int _dryer_speed;
+
+static int console_dryer(int argc, char **argv)
+{
+  ESP_LOGI(TAG,"Current speed is %d",_dryer_speed);
+
+  new_speed = atoi(argv[1]);
+  ESP_ERROR_CHECK(ledc_set_duty(LEDC_MODE, LEDC_CHANNEL, _dryer_speed));
+  ESP_ERROR_CHECK(ledc_update_duty(LEDC_MODE, LEDC_CHANNEL));
+  return 0;
+}
+
+void console_init()
+  {
+    esp_console_repl_t *repl = NULL;
+    esp_console_repl_config_t repl_config = ESP_CONSOLE_REPL_CONFIG_DEFAULT();
+    /* Prompt to be printed before each line.
+     * This can be customized, made dynamic, etc.
+     */
+    repl_config.prompt = PROMPT_STR ">";
+    repl_config.max_cmdline_length = 1024;
+
+    esp_console_register_help_command();
+    register_system_common();
+
+    const esp_console_cmd_t cmd = {
+      .command = "dryer",
+      .help = "dryer control",
+      .hint = NULL,
+      .func = &console_dryer,
+    };
+    ESP_ERROR_CHECK( esp_console_cmd_register(&cmd) );
+
+    esp_console_dev_uart_config_t hw_config = ESP_CONSOLE_DEV_UART_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK(esp_console_new_repl_uart(&hw_config, &repl_config, &repl));
+
+    ESP_ERROR_CHECK(esp_console_start_repl(repl));
+
+  }
+
+static void ledc_init(void)
+{
+  // Prepare and then apply the LEDC PWM timer configuration
+  ledc_timer_config_t ledc_timer = {
+    .speed_mode       = LEDC_MODE,
+    .duty_resolution  = LEDC_DUTY_RES,
+    .timer_num        = LEDC_TIMER,
+    .freq_hz          = LEDC_FREQUENCY,  // Set output frequency at 4 kHz
+    .clk_cfg          = LEDC_AUTO_CLK
+  };
+  ESP_ERROR_CHECK(ledc_timer_config(&ledc_timer));
+
+  // Prepare and then apply the LEDC PWM channel configuration
+  ledc_channel_config_t ledc_channel = {
+    .speed_mode     = LEDC_MODE,
+    .channel        = LEDC_CHANNEL,
+    .timer_sel      = LEDC_TIMER,
+    .intr_type      = LEDC_INTR_DISABLE,
+    .gpio_num       = LEDC_OUTPUT_IO,
+    .duty           = 0, // Set duty to 0%
+    .hpoint         = 0
+  };
+  ESP_ERROR_CHECK(ledc_channel_config(&ledc_channel));
+}
 
 void app_main(void)
 {
-    ESP_LOGI(TAG, "Starting example");
+  console_init();
+    ESP_LOGI(TAG, "!DRYER!");
     ESP_ERROR_CHECK(nvs_flash_init());
     ESP_ERROR_CHECK(esp_netif_init());
     ESP_ERROR_CHECK(esp_event_loop_create_default());
@@ -47,4 +111,10 @@ void app_main(void)
     /* Start the file server */
     ESP_ERROR_CHECK(example_start_file_server(base_path));
     ESP_LOGI(TAG, "File server started");
+    /* Start dryer task */
+    {
+      ledc_init();
+      ESP_ERROR_CHECK(ledc_set_duty(LEDC_MODE, LEDC_CHANNEL, LEDC_DUTY));
+      ESP_ERROR_CHECK(ledc_update_duty(LEDC_MODE, LEDC_CHANNEL));
+    }
 }
